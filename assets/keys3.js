798a804adf6a2e375190b76057ee5dffe7e452b7
MattGaarder <matteus.gaarder@gmail.com>
2023-02-09 14:31:06 +0000

    push without ignore

---
+++ assets/keys3.js
@@ -0,0 +1,6 @@
+var OMDBKey = "407da853";
+// var compromisedYoutubeApiKey = "AIzaSyArZC4w_ZrUpGsejJW0TBItsl8bm2JQMTs"; //compromised
+// var compromisedYoutubeApiKey2 = "AIzaSyAe24J-pt7WWNHFvLHpof0uEOP3liLg4dk";
+var OMDBKey = "407da853"; 
+// var youtubeApiKey (tired) = "AIzaSyAw7R_5sHFdALiY0BUgiwLGdbsIEvyMNOw" //tired
+var youtubeApiKey = "AIzaSyA1-PgZ7gNMfgr_3Fp39k3iP9K7uYMKQbo"
